cf27197ef684d5404d8498a9513d05e090d8a702
xlgwr <xlgwr@ubuntu>
2013-12-15 22:32:29 +0800

    update Task-e finishing the Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,13 +28,14 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
 #    @line_item = LineItem.new(line_item_params)
-    @line_item = @cart.line_items.build(product: product)
+#    @line_item = @cart.line_items.build(product: product)
+     @line_item=@cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
 #        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.html { redirect_to @line_item.cart,
-                     notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
+#                     notice: 'Line item was successfully created.' }
        format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
@@ -75,6 +76,7 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+#      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
+   end
 end
